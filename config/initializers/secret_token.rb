29d13cc2cf33e8927c0d3a98bcf7afc212407f3b
LMCJ <pair1@DevPair.(none)>
2013-05-31 00:07:05 -0400

    LMCJ - create rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Huckleberry::Application.config.secret_token = 'f4225dcdedc94c4f28847d715aeb637f709a9684a4137266d8e3ce258cface373618455cd64829fd4d7a1fb89759e51d8b34d94db26bfa642a7cd2f56ad24c0b'
