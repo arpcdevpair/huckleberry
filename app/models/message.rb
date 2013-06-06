92b80f5acee213381161757bc3354b45a82aa084
LMCJ <pair1@DevPair.(none)>
2013-06-06 01:57:35 -0400

    LMCJ - #4 - Fix formatting

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,6 @@
 class Message < ActiveRecord::Base
   attr_accessible :text, :recipient, :sender
-    belongs_to :sender, class_name: 'User'
-    validates :sender, presence: { message: 'The message requires a sender' }
-    has_many :recipients
+  belongs_to :sender, class_name: 'User'
+  validates :sender, presence: { message: 'The message requires a sender' }
+  has_many :recipients
 end
