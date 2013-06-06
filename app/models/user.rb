0ac26d82eb8cb1a624c61b419a608aa2b4e91fab
LMCJ <pair1@DevPair.(none)>
2013-06-06 01:55:20 -0400

    LMCJ - #4 - Add initials to users for identifying users in text messages.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,12 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :name
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :initials
   # attr_accessible :title, :body
+
+  validates :name, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :initials, presence: true, uniqueness: true
 
   has_many :messages
   has_many :recipients
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130604034435) do
+ActiveRecord::Schema.define(:version => 20130606040021) do
 
   create_table "messages", :force => true do |t|
     t.string   "text"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(:version => 20130604034435) do
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
     t.string   "name"
+    t.string   "initials"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
