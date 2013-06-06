29225f0814ee72474f1a3ded8b138b58756da98e
LMCJ <pair1@DevPair.(none)>
2013-06-06 01:58:25 -0400

    LMCJ - #4 - Add method to return comma delimited list of names for a messages recipients.

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,8 @@ class Message < ActiveRecord::Base
   belongs_to :sender, class_name: 'User'
   validates :sender, presence: { message: 'The message requires a sender' }
   has_many :recipients
+  
+  def list_of_recipients
+    recipients.map(&:user).map(&:name).join ", "
+  end
 end
