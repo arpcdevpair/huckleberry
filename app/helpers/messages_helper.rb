85210d8d53637ed5e7333ebd16d902bf772fb3b2
LMCJ <pair1@DevPair.(none)>
2013-06-08 18:53:23 -0400

    LM - #4, #3 - Link users in messages to profiles

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,2 +1,13 @@
 module MessagesHelper
+  def to_html message
+    html = message.text
+    
+    message.senders.map(&:user).each { |user| html = html.gsub(/#{user.initials}\>/i, "#{profile_link user}&gt;") }
+    message.recipients.map(&:user).each { |user| html = html.gsub(/\@#{user.initials}/i, "@#{profile_link user}") }
+    return html
+  end
+  
+  def profile_link user
+    "#{link_to user.name, user}"
+  end
 end
