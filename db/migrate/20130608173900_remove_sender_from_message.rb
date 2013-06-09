1457e01e09c8f2e45c7e5dd3890c0caf8cbf6699
LMCJ <pair1@DevPair.(none)>
2013-06-09 02:45:51 -0400

    LMCJ - Fix removal of senders column in messages

--- db/migrate/20130608173900_remove_sender_from_message.rb
+++ db/migrate/20130608173900_remove_sender_from_message.rb
@@ -1,10 +1,9 @@
 class RemoveSenderFromMessage < ActiveRecord::Migration
   def up
-    remove_column :messages, :sender
+    remove_column :messages, :sender_id
   end
 
   def down
     raise ActiveRecord::IrreversibleMigration
-    add_column :message, :sender, :references
   end
 end
