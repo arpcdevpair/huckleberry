7106a8f2201476e744e68541ddf649ca83d43c4b
LMCJ <pair1@DevPair.(none)>
2013-06-13 01:18:17 -0400

    LM - #1 - GH00001 - Update message requests to use authentication determining where request will be directed.

--- spec/requests/messages_spec.rb
+++ spec/requests/messages_spec.rb
@@ -1,11 +1,43 @@
 require 'spec_helper'
+include Warden::Test::Helpers
 
 describe "Messages" do
   describe "GET /messages" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get messages_path
-      response.status.should be(200)
+    describe "unauthenticated" do
+      it "redirects to new user session" do
+        get messages_path
+        response.should redirect_to(new_user_session_url)
+      end
+    end
+    describe "approved" do
+      it "shows messages" do
+        login_as users(:approved)
+        get messages_path
+        response.should be_ok
+      end
+      after do
+        Warden.test_reset!
+      end
+    end
+    describe "admin" do
+      it "shows messages" do
+        login_as users(:admin)
+        get messages_path
+        response.should be_ok
+      end
+      after do
+        Warden.test_reset!
+      end
+    end
+    describe "pending approval" do
+      it "redirects to new user session" do
+        login_as users(:unapproved)
+        get messages_path
+        response.should redirect_to(new_user_session_url)
+      end
+      after do
+        Warden.test_reset!
+      end
     end
   end
 end
