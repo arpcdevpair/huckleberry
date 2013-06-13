4eff067a7bd2be25bd2c0d38486f1cc40dccf4ba
LMCJ <pair1@DevPair.(none)>
2013-06-13 01:19:11 -0400

    LM - #1 - GH00001 - Adjust message routing to handle correct paths that messages will be handled. Verified removed routes no longer map.

--- spec/routing/messages_routing_spec.rb
+++ spec/routing/messages_routing_spec.rb
@@ -1,35 +1,31 @@
-require "spec_helper"
+require 'spec_helper'
 
 describe MessagesController do
+  
+  let(:valid_attributes) { { "text" => "MyString" } }
+  
   describe "routing" do
-
     it "routes to #index" do
       get("/messages").should route_to("messages#index")
     end
 
-    it "routes to #new" do
-      get("/messages/new").should route_to("messages#new")
-    end
-
     it "routes to #show" do
-      get("/messages/1").should route_to("messages#show", :id => "1")
-    end
-
-    it "routes to #edit" do
-      get("/messages/1/edit").should route_to("messages#edit", :id => "1")
+      message = Message.create! valid_attributes
+      get("/messages/1").should route_to("messages#show", id: "1")
     end
 
     it "routes to #create" do
       post("/messages").should route_to("messages#create")
     end
 
-    it "routes to #update" do
-      put("/messages/1").should route_to("messages#update", :id => "1")
+    it "should not be route to #edit" do
+      expect(put: "/messages/1").not_to be_routable
     end
-
-    it "routes to #destroy" do
-      delete("/messages/1").should route_to("messages#destroy", :id => "1")
+    it "should not route to #update" do
+      expect(put: "/messages/1").not_to be_routable
     end
-
-  end
+    it "should not route to #destroy" do
+      expect(delete: "/messages/1").not_to be_routable
+    end
+    end
 end
