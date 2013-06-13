1e18fc7a6bcbb14d055d4767207c2add8c802c49
LMCJ <pair1@DevPair.(none)>
2013-06-13 01:20:30 -0400

    LM - #1 - GH00001 - Updated test for index view to account for channels, users, and new message displayed on the main page.

--- spec/views/messages/index.html.erb_spec.rb
+++ spec/views/messages/index.html.erb_spec.rb
@@ -3,13 +3,12 @@ require 'spec_helper'
 describe "messages/index" do
   before(:each) do
     assign(:messages, [
-      stub_model(Message,
-        :text => "Text"
-      ),
-      stub_model(Message,
-        :text => "Text"
-      )
+      stub_model(Message, text: "Text", created_at: Time.now),
+      stub_model(Message, text: "Text", created_at: Time.now) 
     ])
+    assign(:channels, [stub_model(Channel, :name => "Name")])
+    assign(:users, [stub_model(User, :name => "Name")])
+    assign(:message, stub_model(Message, text: "", errors: []))
   end
 
   it "renders a list of messages" do
