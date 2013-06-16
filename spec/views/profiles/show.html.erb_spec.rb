7a88a9c698821e6c934f49c2902c6691340fc25d
Lewis Moten <lewismoten@gmail.com>
2013-06-16 03:15:16 -0400

    LM - #2 - Create reusable message template
    Moved redundant code into its own template and referenced it from
    channels, messages, and profiles.

--- spec/views/profiles/show.html.erb_spec.rb
+++ spec/views/profiles/show.html.erb_spec.rb
@@ -1,23 +1,30 @@
 require 'spec_helper'
 
-describe "profiles/show" do
+describe 'profiles/show' do
   before(:each) do
-    assign(:user, stub_model(User, name:'Bryant', gravatar_hash: 'my_hash', online?: false))
-    assign(:channels, [stub_model(Channel, :name => "Name")])
-    assign(:users, [stub_model(User, :name => "Name")])
-    assign(:message, stub_model(Message, text: "", errors: []))
+    assign(:user, stub_model(User, name:'Bryant', gravatar_hash: 'my_hash', online?: false,
+      sent_messages: [ stub_model(Message, text: 'sent message 1', created_at: Time.now), stub_model(Message, text: 'sent message 2', created_at: Time.now)],
+      received_messages: [ stub_model(Message, text: 'received message 1', created_at: Time.now), stub_model(Message, text: 'received message 2', created_at: Time.now)]))
+    assign(:channels, [stub_model(Channel, :name => 'Name')])
+    assign(:users, [stub_model(User, :name => 'Name')])
+    assign(:message, stub_model(Message, text: '', errors: []))
   end
 
-  it "displays the name" do
+  it 'displays the name' do
     render
-    assert_select "span#Name", text: "Bryant".to_s, count: 1
+    assert_select "span#name", text: "Bryant".to_s, count: 1
   end
-  
+
   it "has an image representing the user" do
     render
     assert_select "img#gravatar", count: 1
   end
-  
+
+  it 'renders a list of sent messages' do
+    render
+    assert_select 'table#sent_messages>tr', :count => 2
+  end
+
   describe "online" do
     before do
       assign(:user, stub_model(User, name:'Bryant', gravatar_hash: 'my_hash', online?: true))
