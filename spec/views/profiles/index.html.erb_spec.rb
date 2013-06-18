5c08a5fda74da13f65fb5316c5c8a3dd89117021
Lewis Moten <lewismoten@gmail.com>
2013-06-18 00:13:22 -0400

    #11 create profile index view

--- spec/views/profiles/index.html.erb_spec.rb
+++ spec/views/profiles/index.html.erb_spec.rb
@@ -1,47 +1,17 @@
 require 'spec_helper'
 
-describe 'profiles/show' do
+describe 'profiles/index' do
   before(:each) do
-    assign(:user, stub_model(User, name:'Bryant', gravatar_hash: 'my_hash', online?: false,
-      sent_messages: [ stub_model(Message, text: 'sent message 1', created_at: Time.now), stub_model(Message, text: 'sent message 2', created_at: Time.now)],
-      received_messages: [ stub_model(Message, text: 'received message 1', created_at: Time.now), stub_model(Message, text: 'received message 2', created_at: Time.now)]))
+    @profile_user_1 = stub_model(User, name: 'John Smith', initials: 'JS')
+    assign(:profile_users, [@profile_user_1])
+
     assign(:channels, [stub_model(Channel, :name => 'Name')])
     assign(:users, [stub_model(User, :name => 'Name')])
     assign(:message, stub_model(Message, text: '', errors: []))
   end
 
-  it 'displays the name' do
+  it 'renders a list of users' do
     render
-    assert_select "span#name", text: "Bryant".to_s, count: 1
-  end
-
-  it "has an image representing the user" do
-    render
-    assert_select "img#gravatar", count: 1
-  end
-
-  it 'renders a list of sent messages' do
-    render
-    assert_select 'table#sent_messages>tr', :count => 2
-  end
-
-  describe "online" do
-    before do
-      assign(:user, stub_model(User, name:'Bryant', gravatar_hash: 'my_hash', online?: true))
-    end
-    it "displays correct status" do
-      render
-      assert_select "span#online", text: "Online!".to_s, count: 1
-    end
-  end
-
-  describe "offline" do
-    before do
-      assign(:user, stub_model(User, name:'Bryant', gravatar_hash: 'my_hash', online?: false))
-    end
-    it "displays correct status" do
-      render
-      assert_select "span#online", text: "Offline".to_s, count: 1
-    end
+    assert_select 'tr>td:nth-of-type(2)', text: @profile_user_1.name, count: 1
   end
 end
